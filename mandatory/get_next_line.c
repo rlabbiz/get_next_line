12d13b1d70c772616c1761dc07ef2931b6e639d1
rida labbiz <redalabbiz@gmail.com>
2024-05-14 17:20:56 +0100

    change the include header file

--- mandatory/get_next_line.c
+++ mandatory/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../includes/get_next_line.h"
 
 char	*ft_strchr(const char *str, int c)
 {
