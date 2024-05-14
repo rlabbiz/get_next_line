b4c2c3248d63d9de2b61952d01d759ca06170838
rida labbiz <redalabbiz@gmail.com>
2024-05-14 17:14:15 +0100

    move mandatory files into mandatory folder

---
+++ mandatory/get_next_line.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlabbiz <rlabbiz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 15:49:03 by rlabbiz           #+#    #+#             */
+/*   Updated: 2022/11/08 18:20:15 by rlabbiz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	char	*s;
+
+	s = (char *)str;
+	while (*s)
+	{
+		if (*s == (char)c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
+
+char	*get_buff(int fd, char *sub)
+{
+	char	*buff;
+	int		i;
+
+	buff = (char *)malloc(sizeof(char ) * BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	i = 1;
+	while (i > 0)
+	{
+		i = read(fd, buff, BUFFER_SIZE);
+		if (i == -1)
+			break ;
+		buff[i] = '\0';
+		sub = ft_strjoin(sub, buff);
+		if (ft_strchr(buff, '\n'))
+			break ;
+	}
+	free(buff);
+	return (sub);
+}
+
+char	*get_line(char *buff)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!buff[0])
+		return (NULL);
+	while (buff[i] && buff[i] != '\n')
+		i++;
+	line = ft_substr(buff, 0, i + 1);
+	return (line);
+}
+
+char	*get_sub(char *buff)
+{
+	char	*sub;
+	int		i;
+
+	i = 0;
+	while (buff[i] && buff[i] != '\n')
+		i++;
+	if (!buff[i])
+	{
+		free(buff);
+		return (NULL);
+	}
+	i++;
+	sub = ft_substr(buff, i, ft_strlen(buff) - i);
+	free(buff);
+	return (sub);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*sub;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	sub = get_buff(fd, sub);
+	if (!sub)
+		return (NULL);
+	line = get_line(sub);
+	sub = get_sub(sub);
+	return (line);
+}
---
+++ mandatory/get_next_line_utils.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlabbiz <rlabbiz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 15:49:07 by rlabbiz           #+#    #+#             */
+/*   Updated: 2022/11/05 15:16:14 by rlabbiz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (start + len > ft_strlen(s))
+		len = ft_strlen(s) - start;
+	substr = malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	ft_memcpy(substr, s + start, len);
+	substr[len] = '\0';
+	return (substr);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (src || dst)
+	{
+		i = 0;
+		while (i < n)
+		{
+			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+	}
+	return (dst);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		size;
+
+	if (!s1)
+		s1 = ft_strdup("");
+	size = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(sizeof(char) * (size + 1));
+	if (!str)
+		return (NULL);
+	ft_memcpy(str, s1, ft_strlen(s1));
+	ft_memcpy(str + ft_strlen(s1), s2, ft_strlen(s2));
+	str[size] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*ft_strdup(char *s1)
+{
+	char	*copy;
+	int		size;
+	int		i;
+
+	if (!s1)
+		return (NULL);
+	i = 0;
+	size = ft_strlen(s1) + 1;
+	copy = malloc(sizeof(char) * size);
+	if (!copy)
+		return (NULL);
+	while (s1[i])
+	{
+		copy[i] = s1[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
