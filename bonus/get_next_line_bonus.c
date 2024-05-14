b2b400164da023d7587309c0c5fe1a9abcd1be8d
rida labbiz <redalabbiz@gmail.com>
2024-05-14 17:22:43 +0100

    change the include header file

--- bonus/get_next_line_bonus.c
+++ bonus/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "../includes/get_next_line_bonus.h"
 
 char	*ft_strchr(const char *str, int c)
 {
